306a2f77bdda475e4928f09bd881408ad31915a9
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2024-06-07 22:08:43 +0700

    format
    Co-authored-by: Kouhakouu <165990461+Kouhakouu@users.noreply.github.com>
    Co-authored-by: khangsilly21 <117421251+khangsilly21@users.noreply.github.com>
    Co-authored-by: bin1st090104 <134790277+bin1st090104@users.noreply.github.com>

--- Practice Project 1 - Tiny Shell/Feature/file.h
+++ Practice Project 1 - Tiny Shell/Feature/file.h
@@ -3,25 +3,30 @@
 #include <fstream>
 #include <iostream>
 
-void writeFile(const std::vector<std::string> &args) {
-    if (args.size() < 2) {
+void writeFile(const std::vector<std::string> &args)
+{
+    if (args.size() < 2)
+    {
         std::cerr << "Usage: echo \"string\" \"filename\"" << std::endl;
         return;
     }
-    
+
     std::string content = args[0];
     std::string filename = args[1];
 
     // Remove quotes from content and filename
-    if (content.front() == '\"' && content.back() == '\"') {
+    if (content.front() == '\"' && content.back() == '\"')
+    {
         content = content.substr(1, content.size() - 2);
     }
-    if (filename.front() == '\"' && filename.back() == '\"') {
+    if (filename.front() == '\"' && filename.back() == '\"')
+    {
         filename = filename.substr(1, filename.size() - 2);
     }
 
     std::ofstream file(filename, std::ios::app);
-    if (!file) {
+    if (!file)
+    {
         std::cerr << "Failed to open file: " << filename << std::endl;
         return;
     }
@@ -29,9 +34,12 @@ void writeFile(const std::vector<std::string> &args) {
     file << content << std::endl;
     file.close();
 
-    if (!file) {
+    if (!file)
+    {
         std::cerr << "Failed to write to file: " << filename << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "Successfully wrote to file: " << filename << std::endl;
     }
 }
--- Practice Project 1 - Tiny Shell/main.cpp
+++ Practice Project 1 - Tiny Shell/main.cpp
@@ -370,7 +370,8 @@ void executeCommand(const std::string &command, const std::vector<std::string> &
             std::cout << "Usage: load_env <filename>" << std::endl;
         }
     }
-    else if(command=="echo"){
+    else if (command == "echo")
+    {
         writeFile(args);
     }
     else
