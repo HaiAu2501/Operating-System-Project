60dd63deb05058db24aa7f26a44500696ea2e338
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2024-06-07 14:50:57 +0700

    fix name

--- Practice Project 1 - Tiny Shell/Feature/suspend_and_resume.h
+++ Practice Project 1 - Tiny Shell/Feature/suspend_and_resume.h
@@ -1,24 +1,26 @@
-#ifndef SUSPEND_AND_RESUME_PROC_H
-#define SUSPEND_AND_RESUME_PROC_H
+#ifndef SUSPEND_AND_RESUME_H
+#define SUSPEND_AND_RESUME_H
 
 #include <vector>
 #include <string>
 #include <iostream>
-
 #include <windows.h>
 #include <tlhelp32.h>
 
 using namespace std;
 
-void suspendProc(const vector<string> &args) {
-    if (args.size() != 1) {
-        cerr << "Usage: suspend_proc <PID>\n";
+void suspendProc(const vector<string> &args)
+{
+    if (args.size() != 1)
+    {
+        cerr << "Usage: suspend <PID>\n";
         return;
     }
     DWORD dwProcessId = stoul(args[0]);
-    
+
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-    if (hSnapshot == INVALID_HANDLE_VALUE) {
+    if (hSnapshot == INVALID_HANDLE_VALUE)
+    {
         cerr << "System error!\n";
         return;
     }
@@ -26,39 +28,46 @@ void suspendProc(const vector<string> &args) {
     THREADENTRY32 te;
     te.dwSize = sizeof(THREADENTRY32);
 
-    if (!Thread32First(hSnapshot, &te)) {
+    if (!Thread32First(hSnapshot, &te))
+    {
         CloseHandle(hSnapshot);
         cerr << "System error!\n";
         return;
     }
 
-    do {
-        if (te.th32OwnerProcessID == dwProcessId) {
+    do
+    {
+        if (te.th32OwnerProcessID == dwProcessId)
+        {
             HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, te.th32ThreadID);
-            if (hThread != NULL) {
+            if (hThread != NULL)
+            {
                 SuspendThread(hThread);
                 CloseHandle(hThread);
             }
-            else {
+            else
+            {
                 cerr << "System error!\n";
                 return;
             }
         }
-    }
-    while (Thread32Next(hSnapshot, &te));
+    } while (Thread32Next(hSnapshot, &te));
 
     CloseHandle(hSnapshot);
 }
 
-void resumeProc(const vector<string> &args) {
-    if (args.size() != 1) {
-        cerr << "Usage: resume_proc <PID>\n";
+void resumeProc(const vector<string> &args)
+{
+    if (args.size() != 1)
+    {
+        cerr << "Usage: resume <PID>\n";
         return;
     }
     DWORD dwProcessId = stoul(args[0]);
-    
+
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-    if (hSnapshot == INVALID_HANDLE_VALUE) {
+    if (hSnapshot == INVALID_HANDLE_VALUE)
+    {
         cerr << "System error!\n";
         return;
     }
@@ -66,26 +75,30 @@ void resumeProc(const vector<string> &args) {
     THREADENTRY32 te;
     te.dwSize = sizeof(THREADENTRY32);
 
-    if (!Thread32First(hSnapshot, &te)) {
+    if (!Thread32First(hSnapshot, &te))
+    {
         CloseHandle(hSnapshot);
         cerr << "System error!\n";
         return;
     }
 
-    do {
-        if (te.th32OwnerProcessID == dwProcessId) {
+    do
+    {
+        if (te.th32OwnerProcessID == dwProcessId)
+        {
             HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, te.th32ThreadID);
-            if (hThread != NULL) {
+            if (hThread != NULL)
+            {
                 ResumeThread(hThread);
                 CloseHandle(hThread);
             }
-            else {
+            else
+            {
                 cerr << "System error!\n";
                 return;
             }
         }
-    }
-    while (Thread32Next(hSnapshot, &te));
+    } while (Thread32Next(hSnapshot, &te));
 
     CloseHandle(hSnapshot);
 }
