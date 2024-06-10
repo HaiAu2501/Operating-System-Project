70db29235371131c984904ceeb1e99a5478823c1
Kouhakouu <phong.bq224887@sis.hust.edu.vn>
2024-06-10 22:52:29 +0700

    Test Base System
    Co-authored-by: khangsilly21 <117421251+khangsilly21@users.noreply.github.com>
    Co-authored-by: bin1st090104 <134790277+bin1st090104@users.noreply.github.com>

---
+++ Testcase/BaseSystem.bat
@@ -0,0 +1,9 @@
+convert 123 from 10 to 2
+convert 234 from 10 to 16
+convert 999 from 10 to 16
+convert 3E7 from 16 to 10
+convert 321 from 10 to 3
+convert 101010010101 from 2 to 10
+convert FFF from 16 to 10
+convert FFF from 16 to 2
+convert 2024 from 10 to 16
